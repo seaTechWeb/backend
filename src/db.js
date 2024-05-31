ab24a74f9ef293d328f1707969f4296f9ab30c99
Ved Prakash <vedprakash182001@gmail.com>
2024-05-31 14:01:04 +0530

    db

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const db = process.env.DB
-mongoose.connect("mongodb+srv://vedprakash182001:EhJ1Ve0jSUIAPJeb@cluster0.wibqnb5.mongodb.net/",{
+mongoose.connect(db,{
 }).then(()=>{
     console.log("Connection Successfull")
 }).catch(()=>{
